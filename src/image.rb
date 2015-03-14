a6e697d12c10f798a4f10bfde119f41964166b80
royceda <caucricity@gmail.com>
2015-03-14 15:21:17 +0100

    image

---
+++ src/image.rb
@@ -0,0 +1,28 @@
+require  'json'
+
+module Weather
+  class Image
+    
+    attr_reader :title
+    attr_reader :width
+    attr_reader :height
+    attr_reader :link
+    attr_reader :url
+
+    def initialize
+    end
+
+
+    def initialize(query)
+      @title  = query["results"]["channel"]["image"]["title"];
+      @width  = query["results"]["channel"]["image"]["width"]; 
+      @height = query["results"]["channel"]["image"]["height"];
+      @link   = query["results"]["channel"]["image"]["link"];
+      @url    = query["results"]["channel"]["image"]["url"];
+
+#      puts @sunrise;
+    end
+
+  end
+end
+
